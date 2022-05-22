a770675b2c747fe25860b171d882b42b3a376c7a
harshit-katyal <hkatyal345@gmail.com>
2022-05-23 00:35:03 +0530

    modified

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -4,8 +4,8 @@ const uploadPic = async media => {
   try {
     const form = new FormData();
     form.append("file", media);
-    form.append("upload_preset", "social_media");
-    form.append("cloud_name", "harshit-katyal");
+    form.append("upload_preset", "");
+    form.append("cloud_name", "");
 
     const res = await axios.post(process.env.CLOUDINARY_URL, form);
     return res.data.secure_url;
